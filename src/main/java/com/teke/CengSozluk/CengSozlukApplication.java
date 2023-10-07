1f25916acac3a6464c3fef71d4da86de87705185
Berk <berkteke16@gmail.com>
2023-10-08 00:01:29 +0300

    Initial commit

---
+++ src/main/java/com/teke/CengSozluk/CengSozlukApplication.java
@@ -0,0 +1,13 @@
+package com.teke.CengSozluk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CengSozlukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CengSozlukApplication.class, args);
+	}
+
+}
