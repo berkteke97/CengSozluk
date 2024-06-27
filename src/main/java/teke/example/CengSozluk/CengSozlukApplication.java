de5bf136f09774e811961f6761ffe6be3912b031
Berk <berkteke16@gmail.com>
2024-06-27 20:52:15 +0300

    First Commit

---
+++ src/main/java/teke/example/CengSozluk/CengSozlukApplication.java
@@ -0,0 +1,16 @@
+package teke.example.CengSozluk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CengSozlukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CengSozlukApplication.class, args);
+	}
+
+
+
+
+}
---
+++ src/test/java/teke/example/CengSozluk/CengSozlukApplicationTests.java
@@ -0,0 +1,13 @@
+package teke.example.CengSozluk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CengSozlukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
