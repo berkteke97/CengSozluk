d549a95625fba716d76b28c2e5987e686ebc19e2
Talat Gedikli <atalatgedikli@gmail.com>
2024-07-07 02:00:16 +0300

    Topic Model created.

---
+++ src/main/java/com/teke/CengSozluk/model/Topic.java
@@ -0,0 +1,29 @@
+package com.teke.CengSozluk.model;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@JsonPropertyOrder({
+        "id",
+        "title",
+        "created_at",
+        "created_by",
+})
+@Data
+@Entity
+
+
+public class Topic {
+    @Id
+    private UUID id;
+
+    private String title;
+
+    private LocalDateTime created_at;
+
+    private UUID created_by;
+}
