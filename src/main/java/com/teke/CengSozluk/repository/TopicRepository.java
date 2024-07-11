3cbbfaca1492709ee512e24cf53552290ae5397e
Talat Gedikli <atalatgedikli@gmail.com>
2024-07-11 20:16:34 +0300

    TopicDTO, TopicRepository created.

---
+++ src/main/java/com/teke/CengSozluk/repository/TopicRepository.java
@@ -0,0 +1,14 @@
+package com.teke.CengSozluk.repository;
+
+import com.teke.CengSozluk.model.Topic;
+import com.teke.CengSozluk.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+
+public interface TopicRepository extends CrudRepository<Topic, UUID> {
+
+    Topic findById(UUID id);
+}
